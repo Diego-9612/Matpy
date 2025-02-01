d26db51fba599e130fb79e159500a9a24f569053
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-01-31 22:52:40 -0500

    se refactoriza todo el codigo a poo

--- script.js
+++ script.js
@@ -11,49 +11,74 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
+class App {
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    #map;
+    #mapEvent;
+
+    constructor() {
+        this._getPosition();
+
+        form.addEventListener('submit', this._newWorkout.bind(this));
+
+        inputType.addEventListener('change', this._toggleElevationField);
+
+    };
+
+    _getPosition() {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+                function () {
+                    alert('Could not get your position');
+                });
+        };
+    };
+
+    _loadMap(position) {
+
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         const coords = [latitude, longitude];
 
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
-        map.on('click', function (mapE) {
-            mapEvent = mapE;
+
+        this.#map.on('click', this._showForm.bind(this));
+    };
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
             form.classList.remove('hidden');
             inputDistance.focus();
+    };
 
-        });
-    });
+    _toggleElevationField() {
+
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+
+    };
+
+    _newWorkout(e) {
+        e.preventDefault();
+
+        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ' ';
+
+        const { lat, lng } = this.#mapEvent.latlng;
+        L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
+            maxWidth: 300,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }).setContent('Workup')).openPopup();
+    };
 };
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = ' ';
-
-    const { lat, lng } = mapEvent.latlng;
-    L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
-        maxWidth: 300,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup'
-    }).setContent('Workup')).openPopup();
-});
-
-inputType.addEventListener('change', function(e){
-    e.preventDefault();
-
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');  
-});
-
+const app = new App();
